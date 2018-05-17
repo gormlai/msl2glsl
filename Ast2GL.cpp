774adb7ab6e8d8de5fdfb2da0a64056304d991ea
Gorm Lai <gorm@kotoristudios.com>
2018-05-17 15:10:00 +0100

    added basic scaffolding for Transpiler

--- Ast2GL.cpp
+++ Ast2GL.cpp
@@ -1,17 +1,10 @@
 #include "Ast2GL.h"
 #include "ShaderCollector.h"
+#include "Transpiler.h"
 
-void Ast2GL::convert(Block * block)
+namespace
 {
-  if(block == nullptr) {
-    printf("Ast2Gl::convert -> program pointer is null");
-  }
-
-  /// collecting shaders to be converted
-  ShaderCollector sCollector;
-  printf("Collecting Shaders ... \n");
-  _shaders = sCollector.collect(block);
-  for(FunctionDeclaration * fDecl : _shaders)
+  std::string simplifiedFunctionSignature(FunctionDeclaration * fDecl)
   {
     std::string output = fDecl->_returnType + " " + fDecl->_name + "(...)";
     switch(fDecl->_functionType)
@@ -25,8 +18,35 @@ void Ast2GL::convert(Block * block)
     default:
       break;
     }
+    return output;
+  }
+}
 
+void Ast2GL::convert(Block * block)
+{
+  if(block == nullptr) {
+    printf("Ast2Gl::convert -> program pointer is null");
+  }
+
+  /// collecting shaders to be converted
+  ShaderCollector sCollector;
+  printf("Collecting Shaders ... \n");
+  _shaders = sCollector.collect(block);
+  for(FunctionDeclaration * fDecl : _shaders)
+  {
+    const std::string output = simplifiedFunctionSignature(fDecl);
     printf("\t%s\n", output.c_str());
+  }
+  printf("\n");
+
+  // convert shaders
+  printf("Converting and Writing Shaders\n");
+  for(FunctionDeclaration * fDecl : _shaders)
+  {
+    const std::string output = simplifiedFunctionSignature(fDecl);
+    printf("\tConverting shaders: %s\n", output.c_str() );
+    
+    
   }
   
   
