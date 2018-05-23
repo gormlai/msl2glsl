fdf91dedce4a912e048e65f6713ec1a66256ec86
Gorm Lai <gorm@kotoristudios.com>
2018-05-23 12:41:10 +0100

    simplified shader collector

--- ShaderCollector.cpp
+++ ShaderCollector.cpp
@@ -2,123 +2,23 @@
 
 std::vector<struct FunctionDeclaration *> ShaderCollector::collect(Block * root)
 {
-  _shaders.clear();
+  std::vector<struct FunctionDeclaration *> shaders;
   
-  if(root!=nullptr)
-    root->visit(this);
-
-  return _shaders;
-  
-}
-
-
-void ShaderCollector::operateOn(struct AssignStatement * desc)
-{
-  desc->_left->visit(this);
-  desc->_right->visit(this);
-}
-
-void ShaderCollector::operateOn(struct BinaryExpression * desc)
-{
-  desc->_left->visit(this);
-  desc->_right->visit(this);
-}
-
-void ShaderCollector::operateOn(struct Block * block)
-{
-  for(auto node : block->_nodes)
-    node->visit(this);
-}
-
-void ShaderCollector::operateOn(struct BufferDescriptor * desc)
-{
-  // leaf node
-}
-
-void ShaderCollector::operateOn(struct ConstantExpression * desc)
-{
-  // leaf node  
-}
-
-void ShaderCollector::operateOn(struct Expression * desc)
-{
-  // leaf node
-}
-
-void ShaderCollector::operateOn(struct FunctionCall * node)
-{
-  if(node->_arguments!=nullptr)
-    node->_arguments->visit(this);
-}
-
-void ShaderCollector::operateOn(struct FunctionCallArgumentList * node)
-{
-  for(auto arg : node->_expressions)
-    arg->visit(this);
-}
-
-void ShaderCollector::operateOn(struct FunctionDeclaration * node)
-{
-  // leaf node
-  switch(node->_functionType)
-    {
-    case FunctionType::Vertex:
-    case FunctionType::Fragment:
-      _shaders.push_back(node);
-      break;
-    default:
-      break;
+  for(auto node : root->_nodes) {
+    if(node->getNodeType() == NodeType::FunctionDeclaration) {
+      FunctionDeclaration * function = (FunctionDeclaration*)node;
+      switch(function->_functionType) {
+      case FunctionType::Vertex:
+      case FunctionType::Fragment:
+	shaders.push_back(function);
+	break;
+      default:
+	break;
+      }
     }
+  }
+  return shaders;
+  
 }
 
-void ShaderCollector::operateOn(struct Node * node)
-{
-  // leaf node
-}
 
-void ShaderCollector::operateOn(struct Program * program)
-{
-  for(auto node : program->_nodes)
-    node->visit(this);
-}
-
-void ShaderCollector::operateOn(struct ReturnStatement * statement)
-{
-  if(statement->_expression != nullptr)
-    statement->_expression->visit(this);
-}
-
-void ShaderCollector::operateOn(struct Statement * statement)
-{
-  // leaf node
-}
-
-void ShaderCollector::operateOn(struct Struct * strct)
-{
-  strct->_block.visit(this);
-}
-
-void ShaderCollector::operateOn(struct UnaryExpression * desc)
-{
-  desc->_expression->visit(this);
-}
-
-void ShaderCollector::operateOn(struct UsingDeclaration * usingDecl)
-{
-  // leaf node
-}
-
-void ShaderCollector::operateOn(struct VariableAttribute * node)
-{
-  // leaf node
-}
-
-void ShaderCollector::operateOn(struct VariableDeclaration * node)
-{
-  // leaf node
-}
-
-void ShaderCollector::operateOn(struct VariableList * node)
-{
-  // leaf node
-}
--- ShaderCollector.h
+++ ShaderCollector.h
@@ -1,41 +1,14 @@
 #ifndef _SHADER_COLLECTOR_H_
 #define _SHADER_COLLECTOR_H_
 
-#include "Visitor.h"
 #include "Types.h"
 
 #include <vector>
 
-class ShaderCollector : public Visitor
+class ShaderCollector
 {
  public:
-  std::vector<struct FunctionDeclaration *>  collect(Block * root);
-  virtual ~ShaderCollector() {}
-  
-  void operateOn(struct AssignStatement * desc) override; 
-  void operateOn(struct BinaryExpression * desc) override;
-  void operateOn(struct Block * block) override;
-  void operateOn(struct BufferDescriptor * desc) override;
-  void operateOn(struct ConstantExpression * desc) override;  
-  void operateOn(struct Expression * desc) override;  
-  void operateOn(struct FunctionCall * node) override;
-  void operateOn(struct FunctionCallArgumentList * node) override;
-  void operateOn(struct FunctionDeclaration * node) override;
-  void operateOn(struct Node * node) override;  
-  void operateOn(struct Program * program) override;
-  void operateOn(struct ReturnStatement * statement) override;
-  void operateOn(struct Statement * statement) override;
-  void operateOn(struct Struct * strct) override;
-  void operateOn(struct UnaryExpression * desc) override;  
-  void operateOn(struct UsingDeclaration * usingDecl) override;
-  void operateOn(struct VariableAttribute * node) override;
-  void operateOn(struct VariableDeclaration * node) override;  
-  void operateOn(struct VariableList * node) override;  
-
- private:
-  std::vector<struct FunctionDeclaration *> _shaders;
-
-  
+  static std::vector<struct FunctionDeclaration *>  collect(Block * root);
 };
 
 
